78ac466fb49cbed703cd4b884251dbb01d580084
caozilu <caozilu@outlook.com>
2020-01-13 20:14:44 +0800

    Create 3.2 PermMissingElem.js
    use Set to find missing element

---
+++ 3.2 PermMissingElem.js
@@ -0,0 +1,14 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    var mySet = new Set(A);
+    
+    for(var i = 1; i < A.length + 2; i++){
+        if(!mySet.has(i)){
+            return i;
+        }
+    }
+
+}
