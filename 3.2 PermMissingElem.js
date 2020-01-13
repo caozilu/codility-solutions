55268af3448197bac0c57c0e56dac4a48d0cfa53
caozilu <caozilu@outlook.com>
2020-01-13 23:37:57 +0800

    Update 3.2 PermMissingElem.js
    refine naming

--- 3.2 PermMissingElem.js
+++ 3.2 PermMissingElem.js
@@ -3,10 +3,10 @@
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
-    var mySet = new Set(A);
-    
+    var permutation = new Set(A);
+
     for(var i = 1; i < A.length + 2; i++){
-        if(!mySet.has(i)){
+        if(!permutation.has(i)){
             return i;
         }
     }
